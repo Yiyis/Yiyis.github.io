73f26711f897ecb9b715c4111d19c92f1185da59
Yiyi <414956851@qq.com>
2019-09-11 23:59:19 -0400

    Adding loader

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,26 +1,28 @@
-var $wrap = $('.wrap').isotope({
-  itemSelector: '.grid-item',
-    percentPosition: true,
-    masonry: {
-        columnWidth: '.grid-sizer'
-    }
+var $wrap = $(".wrap").isotope({
+  itemSelector: ".grid-item",
+  percentPosition: true,
+  masonry: {
+    columnWidth: ".grid-sizer"
+  }
 });
 
-
-$('.filters-button-group').on( 'click', 'button', function() {
-  var filterValue = $( this ).attr('data-filter');
+$(".filters-button-group").on("click", "button", function() {
+  var filterValue = $(this).attr("data-filter");
   $wrap.isotope({ filter: filterValue });
 });
 
-
-$('.button-group').each( function( i, buttonGroup ) {
-  var $buttonGroup = $( buttonGroup );
-  $buttonGroup.on( 'click', 'button', function() {
-    $buttonGroup.find('.active').removeClass('active');
-    $( this ).addClass('active');
+$(".button-group").each(function(i, buttonGroup) {
+  var $buttonGroup = $(buttonGroup);
+  $buttonGroup.on("click", "button", function() {
+    $buttonGroup.find(".active").removeClass("active");
+    $(this).addClass("active");
   });
 });
 
-$wrap.imagesLoaded().progress( function() {
-  $wrap.isotope('layout');
+$wrap.imagesLoaded().progress(function() {
+  $wrap.isotope("layout");
+});
+
+$(window).on("load", function() {
+  $(".loader-wrapper").fadeOut("slow");
 });
