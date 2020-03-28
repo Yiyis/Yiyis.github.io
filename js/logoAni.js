f6870ae62ca1be0780d1e4136249d8a56e571439
Yiyi <414956851@qq.com>
2020-03-28 14:15:55 -0400

    Replaced nav bar on about page

---
+++ js/logoAni.js
@@ -0,0 +1,8 @@
+// ----------Logo Animation-------------
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById("bm"),
+  renderer: "svg",
+  loop: false,
+  autoplay: true,
+  path: "data.json"
+});
