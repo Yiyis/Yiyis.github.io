18fa4db989c34d2ae4b468e4e6e5cd3a69b43eb8
Yiyi <414956851@qq.com>
2021-04-06 12:48:25 -0400

    Fixed isotope loading bug

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -4,91 +4,110 @@ import ReactDOM from "react-dom";
 import Isotope from "isotope-layout";
 import data from "../assets/projectsData.json";
 
-function Portfolio() {
-  const isotope = React.useRef();
+class Portfolio extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loaded: false };
+    this.handleFilterKeyChange = this.handleFilterKeyChange.bind(this);
+  }
 
-  const [filterKey, setFilterKey] = React.useState("*");
+  componentDidUpdate() {
+    if (this.isotope === undefined) {
+      this.isotope = new Isotope(".wrap", {
+        itemSelector: ".grid-item",
+        percentPosition: true,
+        masonry: {
+          columnWidth: ".grid-sizer",
+        },
+      });
+    }
+  }
 
-  // initialize an Isotope object with configs
-  React.useEffect(() => {
-    isotope.current = new Isotope(".wrap", {
-      itemSelector: ".grid-item",
-      percentPosition: true,
-      masonry: {
-        columnWidth: ".grid-sizer",
-      },
-    });
-    // cleanup
-    return () => isotope.current.destroy();
-  }, []);
+  handleFilterKeyChange(key) {
+    if (this.isotope === undefined) {
+      this.isotope = new Isotope(".wrap", {
+        itemSelector: ".grid-item",
+        percentPosition: true,
+        masonry: {
+          columnWidth: ".grid-sizer",
+        },
+      });
+    }
+    if (key === "*") {
+      this.isotope.arrange({ filter: `*` });
+    } else {
+      this.setState({ filter: key });
+      this.isotope.arrange({ filter: `.${key}` });
+    }
+  }
 
-  // handling filter key change
-  React.useEffect(() => {
-    filterKey === "*"
-      ? isotope.current.arrange({ filter: `*` })
-      : isotope.current.arrange({ filter: `.${filterKey}` });
-  }, [filterKey]);
-
-  const handleFilterKeyChange = (key) => () => setFilterKey(key);
-  return (
-    <div>
-      <Nav displayLogo={true} />
-      <div className="button-group filters-button-group">
-        <button
-          className="btn-filter active"
-          onClick={handleFilterKeyChange("*")}
-        >
-          All
-        </button>
-        <button
-          className="btn-filter"
-          onClick={handleFilterKeyChange("Software")}
-        >
-          Software Development
-        </button>
-        <button
-          className="btn-filter"
-          onClick={handleFilterKeyChange("Wearables")}
-        >
-          Wearables
-        </button>
-        <button className="btn-filter" onClick={handleFilterKeyChange("MR")}>
-          Mixed Reality
-        </button>
-        <button
-          className="btn-filter"
-          onClick={handleFilterKeyChange("GraphicDesign")}
-        >
-          Graphic Design
-        </button>
-        <button
-          className="btn-filter"
-          onClick={handleFilterKeyChange("Animation")}
-        >
-          Animation
-        </button>
-        <button
-          className="btn-filter"
-          onClick={handleFilterKeyChange("Installation")}
-        >
-          Installation
-        </button>
-      </div>
-      <div className="wrap">
-        <div className="grid-sizer"></div>
-        {data.map((project) => (
-          <div className={`${project.tags} grid-item`} key={project.key}>
-            <img src={project.mainPhoto} alt="" />
-            <div className="info-wrapper">
-              <div className="name">{project.name}</div>
-              <div className="memo">{project.memo}</div>
-              <div className="date">{project.date}</div>
+  render() {
+    return (
+      <div>
+        <Nav displayLogo={true} />
+        <div className="button-group filters-button-group">
+          <button
+            className="btn-filter active"
+            onClick={() => this.handleFilterKeyChange("*")}
+          >
+            All
+          </button>
+          <button
+            className="btn-filter"
+            onClick={() => this.handleFilterKeyChange("Software")}
+          >
+            Software Development
+          </button>
+          <button
+            className="btn-filter"
+            onClick={() => this.handleFilterKeyChange("Wearables")}
+          >
+            Wearables
+          </button>
+          <button
+            className="btn-filter"
+            onClick={() => this.handleFilterKeyChange("MR")}
+          >
+            Mixed Reality
+          </button>
+          <button
+            className="btn-filter"
+            onClick={() => this.handleFilterKeyChange("GraphicDesign")}
+          >
+            Graphic Design
+          </button>
+          <button
+            className="btn-filter"
+            onClick={() => this.handleFilterKeyChange("Animation")}
+          >
+            Animation
+          </button>
+          <button
+            className="btn-filter"
+            onClick={() => this.handleFilterKeyChange("Installation")}
+          >
+            Installation
+          </button>
+        </div>
+        <div className="wrap">
+          <div className="grid-sizer"></div>
+          {data.map((project) => (
+            <div className={`${project.tags} grid-item`} key={project.key}>
+              <img
+                src={project.mainPhoto}
+                alt=""
+                onLoad={() => this.setState({ loaded: true })}
+              />
+              <div className="info-wrapper">
+                <div className="name">{project.name}</div>
+                <div className="memo">{project.memo}</div>
+                <div className="date">{project.date}</div>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
 export default Portfolio;
