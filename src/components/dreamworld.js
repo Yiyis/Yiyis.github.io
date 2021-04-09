786544a2a4ebae93ca4c1865744b0adcaa5a915e
Yiyi <414956851@qq.com>
2021-04-09 19:32:23 -0400

    Finished DreamWorld

--- src/components/dreamworld.js
+++ src/components/dreamworld.js
@@ -14,7 +14,7 @@ class Dreamworld extends React.Component {
           <img
             className="project-main img-fluid cover"
             src="pics/projects/DreamWorld/DreamWorld_L.jpg"
-            alt="VPIA Platform"
+            alt="Dream World AR"
           />
           <div className="img-overlay"></div>
           <h1 className="project-title text-center text-white">
@@ -27,9 +27,65 @@ class Dreamworld extends React.Component {
             role={data[2].role}
           />
         </div>
-        <div id="content"></div>
-        <div className="col text-center pb-5">
-          <ScrollToTop />
+        <div id="content">
+          <div className="container-fluid bg-black p-custom">
+            <div className="row">
+              <div className="col-md-6">
+                <h1 className="text-light">DreamWorld AR</h1>
+                <p className="text-light">
+                  Created for iPhones using Unity3D, ARFoundation, ARKit, XCode,
+                  Adobe Illustrator, Adobe Photoshop
+                </p>
+                <h3 className="pt-5 text-light">Summary</h3>
+                <p className="text-light">
+                  DreamWorld AR is an experimental project exploring the
+                  boundary between virtual and real worlds.
+                </p>
+                <h3 className="pt-5 text-light">Goal</h3>
+                <p className="text-light">
+                  Design an user-friendly interface for the AR app. Inspired by
+                  Vishnu Ganti's UI/UX for hand-held AR and Tevfik Ufuk
+                  DEMİRBAŞ's AR Portal.
+                </p>
+              </div>
+              <div className="col-md-6">
+                <div className="video-wrapper">
+                  <iframe
+                    width="560"
+                    height="315"
+                    title="DreamWorld AR"
+                    src="https://www.youtube.com/embed/_gRENmGkTjg"
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                  ></iframe>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="container-fluid bg-pink p-4">
+            <div className="row">
+              <div className="col-md-8 text-center">
+                <h4>1.User Flow</h4>
+
+                <img
+                  src="pics/projects/DreamWorld/UserFlow.jpg"
+                  className="img-fluid"
+                  alt="user flow diagram"
+                />
+
+                <h4>2.Animation Design</h4>
+
+                <img
+                  src="pics/projects/DreamWorld/UI_2.gif"
+                  className="img-fluid"
+                  alt="UI animation"
+                />
+              </div>
+              <div className="col-12 text-center pb-5">
+                <ScrollToTop />
+              </div>
+            </div>
+          </div>
         </div>
       </motion.div>
     );
