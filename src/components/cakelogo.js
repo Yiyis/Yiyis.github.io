1c138a0fa319c04efe7275fb0e91741ace4e533d
Yiyi <414956851@qq.com>
2021-04-11 15:29:36 -0400

    Finished Cake logo

--- src/components/cakelogo.js
+++ src/components/cakelogo.js
@@ -27,9 +27,44 @@ class CakeLogo extends React.Component {
             role={data[15].role}
           />
         </div>
-        <div id="content"></div>
-        <div className="col text-center pb-5">
-          <ScrollToTop />
+        <div id="content">
+          <div className="container-fluid p-custom">
+            <div className="row">
+              <div className="col-md-6">
+                <h1>Cake Logo Design</h1>
+                <p>
+                  Created using Adobe Illustrator, Adobe Photoshop and
+                  watercolour
+                </p>
+                <h3 className="pt-5 text-dark">Summary</h3>
+                <p>
+                  Bluebell Confectionery logo is designed in a retro and elegant
+                  style with perferred elements such as roses and leaves. It was
+                  hand-drawn with watercolour and then scanned and transferred
+                  into a vector format. The logo can be used as stickers,
+                  packages, social medias, business cards, shop sign and so on.
+                </p>
+                <p className="pt-3">
+                  All copyright of this logo belongs to Bluebell Confectionery.
+                </p>
+              </div>
+              <div className="col-md-6 ">
+                <img
+                  src="pics/projects/CakeLogo/Logo1.jpg"
+                  className="img_fluid w-100"
+                />
+              </div>
+              <div className="col-12 p-0">
+                <img
+                  src="pics/projects/CakeLogo/sketches.jpg"
+                  className="img_fluid w-100"
+                />
+              </div>
+              <div className="col text-center pb-5 pt-5">
+                <ScrollToTop />
+              </div>
+            </div>
+          </div>
         </div>
       </motion.div>
     );
