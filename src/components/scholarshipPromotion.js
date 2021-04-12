70822acbb08b0ee3066fe8fb899c1422dfe207e7
Yiyi <414956851@qq.com>
2021-04-12 14:12:27 -0400

    Added scholarship promotion

--- src/components/scholarshipPromotion.js
+++ src/components/scholarshipPromotion.js
@@ -14,7 +14,7 @@ class ScholarshipPromotion extends React.Component {
           <img
             className="project-main img-fluid cover"
             src="pics/projects/ScholarshipPromotion/ScholarshipPromotionPoster_3.jpg"
-            alt="VPIA Platform"
+            alt="Scholarship Promotion"
           />
           <div className="img-overlay"></div>
           <ProjectInfo
@@ -24,9 +24,47 @@ class ScholarshipPromotion extends React.Component {
             role={data[11].role}
           />
         </div>
-        <div id="content"></div>
-        <div className="col text-center pb-5">
-          <ScrollToTop />
+        <div id="content">
+          <div className="container-fluid p-custom">
+            <div className="row">
+              <div className="col-md-6">
+                <h1>Scholarship Promotion Design</h1>
+                <p>Created using Adobe Illustrator, Adobe InDesign</p>
+                <h3 className="pt-5 text-dark">Summary</h3>
+                <p>
+                  Poster and postcard design for Scholarship Promotion at
+                  Plymouth University
+                </p>
+              </div>
+              <div className="col-md-6 ">
+                <img
+                  src="pics/projects/ScholarshipPromotion/ScholarshipPromotion.png"
+                  className="img_fluid w-100"
+                  alt="poster design"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="container-fluid">
+            <img
+              src="pics/projects/ScholarshipPromotion/ScholarshipPromotionPoster_2.jpg"
+              className="img_medium"
+              alt="poster display"
+            />
+            <img
+              src="pics/projects/ScholarshipPromotion/ScholarshipPromotionPostcard_1.jpg"
+              className="img_medium"
+              alt="flyer display"
+            />
+            <img
+              src="pics/projects/ScholarshipPromotion/ScholarshipPromotionPostcard_2.jpg"
+              className="img_medium"
+              alt="flyer display"
+            />
+          </div>
+          <div className="col text-center pb-5">
+            <ScrollToTop />
+          </div>
         </div>
       </motion.div>
     );
