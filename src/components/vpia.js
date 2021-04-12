3da5acd997baa9d294f99832bf3378d6df313614
Yiyi <414956851@qq.com>
2021-04-12 17:57:26 -0400

    Added VPIA

--- src/components/vpia.js
+++ src/components/vpia.js
@@ -25,9 +25,77 @@ class Vpia extends React.Component {
             role={data[1].role}
           />
         </div>
-        <div id="content"></div>
-        <div className="col text-center pb-5">
-          <ScrollToTop />
+        <div id="content">
+          <div className="container-fluid p-custom">
+            <div className="row">
+              <div className="col-md-6">
+                <h1>Virtual Platform for Indigenous Arts</h1>
+                <p className="pt-3">
+                  This project was led by Dr. Gerald McMaster as part of the
+                  larger Entangled Gaze project at OCAD University’s{" "}
+                  <a href="https://wapatah.com/" className="alert-link">
+                    Wapatah Centre for Indigenous Visual Knowledge
+                  </a>
+                  , the VPIA is designed to facilitate Indigenous access,
+                  interpretation, and learning from artwork collections held by
+                  museums and galleries around the world.
+                </p>
+                <p className="pt-3">
+                  VPIA is a wiki-style platform that allows institutional
+                  artwork records to be transformed into living documents
+                  through the integration of Indigenous knowledge, language, and
+                  protocols
+                </p>
+                <h3 className="pt-5 text-dark">Platfrom stack: </h3>
+                <ul>
+                  <li>
+                    Back-end: Node.js v15.0.0, Express.js 4.17.1 with RESTful
+                    API design, MySQL and Caminte, Progressive Web App (PWA)
+                  </li>
+                  <li>Front-end: React 16.14.0, Bootstrap 4.3.1 and TinyMCE</li>
+                  <li>Version control: Git, hosted on Github</li>
+                  <li>Hosting: AWS, Nginx for reverse proxy</li>
+                  <li>Linting tools: Prettier and ESlint</li>
+                  <li>Bundling: Webpack</li>
+                  <li>PRs testing: LGTM and TravisCI</li>
+                  <li>DevOps: TravisCI and AWS Codeploy</li>
+                  <li>Domain management: Reclaim Hosting/Cpanel</li>
+                </ul>
+                <p className="pt-3">
+                  Visit the plafrom{" "}
+                  <a href="https://vpia.wapatah.com" className="alert-link">
+                    here
+                  </a>
+                </p>
+                <p className="pt-3">
+                  <a
+                    href="https://wapatah.com/virtual-platform-for-indigenous-art/"
+                    className="alert-link"
+                  >
+                    Learn more
+                  </a>{" "}
+                  about the platform
+                </p>
+              </div>
+              <div className="col-md-6">
+                <img
+                  src="pics/projects/VPIA/VPIA.jpg"
+                  className="img_fluid w-100"
+                  alt="VPIA"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="container-fluid bg-green">
+            <img
+              src="pics/projects/VPIA/showcase.jpg"
+              className="img_fluid w-100"
+              alt="VPIA page showcase"
+            />
+            <div className="col text-center pb-5 pt-5">
+              <ScrollToTop />
+            </div>
+          </div>
         </div>
       </motion.div>
     );
